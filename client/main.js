ce6df295bf27a208e13c1fd0d8bc4e95c2f46b9f
Dan Rice <dan.rice.92@outlook.com>
2018-09-03 16:51:18 -0600

    install Reactstrap, Bootstrap

--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 
 import { renderRoutes } from '../imports/startup/client/routes.js';
 import './main.html';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
