51ac048f815cd08459896698b9850da3ffdbdb22
Dan Rice <dan.rice.92@outlook.com>
2018-09-07 08:17:36 -0600

    remove comments, organize imports better

--- client/main.js
+++ client/main.js
@@ -3,18 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
 
-import { renderRoutes } from '../imports/startup/client/routes.js';
 import './main.html';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { renderRoutes } from '../imports/startup/client/routes.js';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
 });
-
-// import { Meteor } from 'meteor/meteor';
-// import { render } from 'react-dom';
-// import { renderRoutes } from '../imports/startup/client/routes.js';
-//
-// Meteor.startup(() => {
-//   render(renderRoutes(), document.getElementById('app'));
-// });
