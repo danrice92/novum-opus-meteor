d57b315f98ad9097dd0f801cd2ffd0dc0aebee72
Dan Rice <dan.rice.92@outlook.com>
2018-10-14 15:41:11 -0600

    Add test, dependencies, README update

--- tests/main.js
+++ tests/main.js
@@ -1,21 +1,40 @@
+import 'jsdom-global/register';
+import React from 'react';
 import assert from "assert";
+import Home from '../imports/ui/pages/Home.js';
 import chai from "chai";
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { spy } from 'sinon';
 
-describe("novum-opus", function () {
-  it("package.json has correct name", async function () {
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('novum-opus', () => {
+  it("has the correct package.json name", async function() {
     const { name } = await import("../package.json");
-    chai.assert.strictEqual(name, "novum-opus");
+    expect(name).to.equal("novum-opus");
   });
 
   if (Meteor.isClient) {
-    it("client is not server", function () {
-      chai.assert.strictEqual(Meteor.isServer, false);
+    it("is not the server", () => {
+      expect(Meteor.isServer).to.equal(false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
-      chai.assert.strictEqual(Meteor.isClient, false);
+    it("is not the client", () => {
+      expect(Meteor.isClient).to.equal(false);
     });
   }
 });
+
+spy(Home.prototype, 'render');
+
+describe('<Home />', () => {
+  it('calls render', () => {
+    const wrapper = mount(<Home />);
+    expect(Home.prototype.render).to.have.property('callCount', 1);
+  })
+})
